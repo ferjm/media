42108a8f60b167457de00f74c47fa314c1f3a9e9
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2018-07-16 18:20:13 +0200

    rustfmt

--- backends/gstreamer/src/lib.rs
+++ backends/gstreamer/src/lib.rs
@@ -1,13 +1,11 @@
-
-
+extern crate gstreamer as gst;
 extern crate gstreamer_app as gst_app;
 extern crate gstreamer_audio as gst_audio;
-extern crate gstreamer as gst;
 
 extern crate servo_media_audio;
 
-extern crate num_traits;
 extern crate byte_slice_cast;
+extern crate num_traits;
 
 use servo_media_audio::AudioBackend;
 
